64026e80e9b59c394a9e36c5c89ab47b61a6fa31
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-05-07 10:22:12 +0300

    feat : validation login

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -11,5 +11,36 @@ class UserController extends BaseController {
         return view('template/login');
     }
 
-    
+    public function validationLogin(){
+        $user = new UserModele(); 
+        $email = $this->request->getPost('email') ; 
+        $pwd = $this->request->getPost('pwd'); 
+        $userBase = $user.getUserByEmail($email)['password'];
+        $pwdBase = $userBase['password'];
+        if($pwdBase === $pwd){
+            $estValid = true ; 
+            $newhash = password_hash($pwd, PASSWORD_DEFAULT);
+            $user->update($userBase['id'], ['password' => $newhash]);
+            
+        }
+        elseif(password_verify($pwd, $pwdBase)){
+            $estValid = true ; 
+        }
+        else{
+            $estValid = false ; 
+        }
+
+        if($estValid){
+                session()->set([
+                    'user_id' => $userBase['id'],
+                    'username' => $userBase['username'],
+                    'email' => $userBase['email'],
+                    'role' => $userBase['role']
+                ]);
+        }
+
+        return redirect()->to('/acceuil');
+    }
+
+   
 }
