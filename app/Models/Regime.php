b23e368078abbc2ddaac10838fc3b1dc1a5f6f1b
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-05-07 20:16:01 +0300

    Feat: Add validation rules and messages for Regime model

--- app/Models/Regime.php
+++ app/Models/Regime.php
@@ -14,6 +14,20 @@ class Regime extends Model
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
 
+    protected $validation = [
+        'name' => 'required|min_length[2]|max_length[100]',
+        'type_variation' => 'required',
+        'variation_poids_jour' => 'required|numeric',
+        'prix_jour' => 'required|numeric'
+    ];
+
+    protected $validationMessages = [
+        'name' => 'Le nom du regime est requis',
+        'type_variation' => 'Le type de la variation est requis',
+        'variation_poids_jour' => 'La variation du poids est requis',
+        'prix_jour' => 'La variation de prix est nécéssaire'
+    ];
+
     protected $allowedFields = ['id','name', 'type_variation', 'variation_poids_jour', 'prix_jour', 'prix_mois', 'prix_3mois', 'prix_6mois', 'prix_12mois'];
 
     public function getRegimeById(int $id)
@@ -25,5 +39,4 @@ class Regime extends Model
     {
         return $this->findAll();
     }
-
 }
