802fa40f3cdd60660394260ab8a43e1c19bd75db
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-05-07 18:40:42 +0300

    feat:ajout route pour validation du login vers accueil

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,11 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/', 'Home::index');
-$routes->get('/acceuil', 'Home::index');
 $routes->get('/login', 'UserController::login');
-$routes->post('/validationLogin', 'UserController::validationLogin');
+$routes->post('/login', 'UserController::validationLogin');
+
+$routes->get('/inscription', 'UserController::inscriptionPage1');
+$routes->post('/step2', 'UserController::inscriptionPage2');
+
+$routes->get('/step2', 'UserController::inscriptionPage2');
+$routes->post('/savePage2', 'UserController::savePage2');   
+
+$routes->post('/register', 'UserController::enregistrementUser');
+
 $routes->get('/logout', 'UserController::logout');
-$routes->get('/inscription','UserController::inscriptionPage1');
-$routes->get('/step2' , 'UserController::inscriptionPage2');
-$routes->post('/savePage2', 'UserController::savePage2');
+$routes->get('/validationLogin','UserController::validationLogin');
