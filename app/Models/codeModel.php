21287664a27d64d8178aa39eebb9bf1d936002e1
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-05-08 13:16:54 +0300

    feat : Creation des CRUD du code

---
+++ app/Models/codeModel.php
@@ -0,0 +1,35 @@
+<?php 
+namespace App\Models;
+use CodeIgniter\Model;
+
+class codeModel extends Model
+{
+    protected $table = 'code';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['code','motant'];
+
+    protected $validationRules = [
+        'code' => 'required|max_length[100]',
+    ];
+
+    function verifCode(string $code): ?array
+    {
+        return $this->where('code', $code)->first();
+    }
+    function getCodeById(int $id): ?array
+    {
+        return $this->find($id);
+    }
+    function updateCode(int $id, array $data): bool
+    {
+        return $this->update($id, $data);
+    }
+    function createCode(array $data): int
+    {
+        $this->insert($data);
+        return $this->getInsertID();
+    }
+    function deleteCode($id){
+        return $this->delete($id);
+    }
+}
