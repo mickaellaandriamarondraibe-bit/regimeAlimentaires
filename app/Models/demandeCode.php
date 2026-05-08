d44eb059c6250254a4cb8d5c5b8a9cb56bbf95bf
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-05-08 14:43:34 +0300

    feat : creation du model de demandede code

---
+++ app/Models/demandeCode.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class demandeCode extends Model
+{
+    protected $table = 'demande_code';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['code_id', 'statut', 'client_id', 'validated_by', 'validated_at'];
+
+    protected $validationRules = [
+        'code_id' => 'required|integer',
+        'statut' => 'required|in_list[en_attente,valide,refuse]',
+        'client_id' => 'required|integer',
+        'validated_by' => 'permit_empty|integer',
+        'validated_at' => 'permit_empty|valid_date',
+    ];
+   
+    function createDemande(array $data): int
+    {
+        $this->insert($data);
+        return $this->getInsertID();
+    }
+    function getDemandeById(int $id): ?array
+    {
+        return $this->find($id);
+    }
+}
