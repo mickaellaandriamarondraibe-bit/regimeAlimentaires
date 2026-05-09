512b44c3099bf8fb58599019ae9ab03958ed232d
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-05-09 10:04:25 +0300

    feat: add initial data for ingredients and objectives in insert_data.sql

--- script-base/insert_data.sql
+++ script-base/insert_data.sql
@@ -3,3 +3,8 @@ INSERT INTO ingredients (name) VALUES
 ('Volaille'),
 ('Viande'),
 ('Poisson');
+
+INSERT INTO objectif (name) VALUES 
+('Perte de poids'),
+('Prise de masse'),
+('Atteinte de l\'IMC idéal');
